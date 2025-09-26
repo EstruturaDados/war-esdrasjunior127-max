cfce901c2a70876e3346de507c2244d34adf36b0
esdrasjunior127-max <esdrasjunior127@gmail.com>
2025-09-26 23:02:50 +0000

    foi colocado as bibliotecas e a struct

---
+++ território.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_TERRITORIOS 5
+#define MAX_STRING 50
+
+// Definição da estrutura
+typedef struct {
+    char nome[MAX_STRING];
+    char cor[MAX_STRING];
+    int tropas;
+} Territorio;
+
