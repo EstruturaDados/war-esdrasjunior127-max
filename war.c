f34c76e32da1920d074d832bcbeeb74e97fcccef
esdrasjunior127-max <esdrasjunior127@gmail.com>
2025-09-27 15:03:26 +0000

    Criado a struct e a funcao de ataque e defensor

--- war.c
+++ war.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_TERRITORIOS 5
-#define MAX_STRING 50
 
 // Definição da estrutura
 typedef struct {
-    char nome[MAX_STRING];
-    char cor[MAX_STRING];
+    char nome[30];
+    char cor[10];
     int tropas;
 } Territorio;
+
+// Função de ataque
+void atacar(Territorio* atacante, Territorio* defensor) {
+    int dadoAtacante = rand() % 6 + 1;
+    int dadoDefensor = rand() % 6 + 1;
+
+    printf("\n--- Batalha ---\n");
+    printf("%s (Atacante) rolou: %d\n", atacante->nome, dadoAtacante);
+    printf("%s (Defensor) rolou: %d\n", defensor->nome, dadoDefensor);
+
+    if (dadoAtacante > dadoDefensor) {
+        printf("Atacante venceu!\n");
+
+        // transfere cor e metade das tropas para o defensor
+        strcpy(defensor->cor, atacante->cor);
+        defensor->tropas = atacante->tropas / 2;
+
+    } else {
+        printf("Defensor resistiu! O atacante perde 1 tropa.\n");
+        if (atacante->tropas > 0) {
+            atacante->tropas -= 1;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main() {
     Territorio territorios[MAX_TERRITORIOS];
@@ -44,3 +82,4 @@ int main() {
 
     return 0;
 }
+
